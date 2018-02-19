7e7f63d4db547de3a9022a6fc521ea6d44a00f5d
Antoine Arnoud <antoine.arnoud@gmail.com>
2018-02-19 19:48:43 +0000

    Update initPointsGenMC.f90
    improved indenting

--- fortran_code/initPointsGenMC.f90
+++ fortran_code/initPointsGenMC.f90
@@ -6,58 +6,62 @@ PROGRAM initPointsGenMC
 
 	IMPLICIT NONE
 
-  CHARACTER(LEN=100) 	:: line
-	REAL								:: mynumber
-	INTEGER							:: p_nx, nmom, maxeval, qr_ndraw, maxpoints, searchType, lotteryPoints
-	REAL, ALLOCATABLE		:: p_bound(:,:)
-	LOGICAL							:: parsedLine
-	INTEGER							:: i, jj
+        CHARACTER(LEN=100) 	:: line
+	REAL			:: mynumber
+	INTEGER			:: p_nx, nmom, maxeval, qr_ndraw, maxpoints, searchType, lotteryPoints
+	REAL, ALLOCATABLE	:: p_bound(:,:)
+	LOGICAL			:: parsedLine
+	INTEGER			:: i, jj, k
+	
 
-
-	! Read dimension and bounds from config file (config.txt)
-  open(UNIT=222, FILE="config.txt", STATUS='old', ACTION='read')
-	! Parse the dimension
-  parsedLine = .FALSE.
+	! Read dimension and bounds from configfile
+        open(UNIT=222, FILE="config.txt", STATUS='old', ACTION='read')
+        
+	!Parse the basic parameters in the first line
+        parsedLine = .FALSE.
 	DO WHILE (parsedLine .eqv. .FALSE.)
-    read(222,'(A100)') line
+            read(222,'(A100)') line
 
-    IF (line(1:1)=='!') THEN  ! cycle as long as line starts with !
-        cycle
-    END IF
-    parsedLine = .TRUE.
-    read(line,*) p_nx, nmom, maxeval, qr_ndraw, maxpoints, searchType, lotteryPoints
+            IF (line(1:1)=='!') THEN
+                cycle
+            END IF
+
+            parsedLine = .TRUE.
+            read(line,*) p_nx, nmom, maxeval, qr_ndraw, maxpoints, searchType, lotteryPoints
 	END DO
 
-	! allocate matrix of lower and upper bounds of the domain
 	allocate(p_bound(p_nx,2))
+	
+	!Parse the bounds
+       	parsedLine = .FALSE.
+        DO WHILE (parsedLine .eqv. .FALSE.)
+            read(222,'(A100)') line
 
-	!Parse the bounds (use the bounds for Sobol points, not for evaluation of function)
- 	parsedLine = .FALSE.
-  DO WHILE (parsedLine .eqv. .FALSE.)
-    read(222,'(A100)') line
+            IF (line(1:1)=='!') THEN
+                cycle
+            END IF
 
-    IF (line(1:1)=='!') THEN   ! cycle as long as line starts with !
-        cycle
-    END IF
-    parsedLine = .TRUE.
-    read(line,*) p_bound(1,1), p_bound(1,2)
-    DO i=2,p_nx
-        read(222,*) p_bound(i,1), p_bound(i,2)
-    END DO
-  END DO
-  close(222)
+            parsedLine = .TRUE.
 
+            read(line,*) p_bound(1,1), p_bound(1,2)
+
+            DO i=2,p_nx
+                read(222,*) p_bound(i,1), p_bound(i,2)
+            END DO
+        END DO
+	
 	! Generate 1000 initial points scaled to the bounds
 	do jj = 1,1000
 	  do i = 1,p_nx
-			! generate a random number between 0 and 1
-	    CALL RANDOM_NUMBER(mynumber)
-			! scale the number to the bounds
-			mynumber = mynumber * (p_bound(i,2) - p_bound(i,1)) + p_bound(i,1)
-			! write the scaled number in dat file
-    	open(unit = 111, file = "init_points.dat", position = "append", STATUS='unknown')
-    		write(111,*) mynumber
-    	close(111)
-		enddo
-  enddo
+		! generate a random number between 0 and 1
+        CALL RANDOM_NUMBER(mynumber)        	
+		! scale the number to the bounds
+		mynumber = mynumber * (p_bound(i,2) - p_bound(i,1)) + p_bound(i,1)
+		! write the scaled number in dat file
+        	open(unit = 111, file = "init_points.dat", position = "append", STATUS='unknown')
+        		write(111,*) mynumber
+        	close(111)
+            close(222)
+	enddo
+      enddo
 END PROGRAM
